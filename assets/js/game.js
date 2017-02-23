049bfcd5993d7ae5fd88f30c4565e0f1abfe2330
epribs <ericpribramsky@gmail.com>
2017-02-22 22:35:50 -0600

    fixed counter and added play/reset buttons

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,25 +1,43 @@
+var wins = 0;
+var loss = 0;
+var counter = 0;
+$("#wins").text(wins);
+$("#loss").text(loss);
+$("#score").text(counter);
 
+var numberOptions = [10, 5, 3, 7];
+for (var i = 0; i < numberOptions.length; i++) {
+  var imageCrystal = $("<img>");
+  imageCrystal.addClass("crystal-image");
+  imageCrystal.attr("src", "assets/img/gem" + [i] + ".png");
+  imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+  $("#crystals").append(imageCrystal);
+}
 
-var targetNumber = Math.floor(Math.random() * (120 - 19)) + 19;
-  $("#number-to-guess").text(targetNumber);
-  var counter = 0;
-  var numberOptions = [10, 5, 3, 7];
-  var wins = 0;
-  var loss = 0;
+function reset(){
+  wins = 0;
+  loss = 0;
+  counter = 0;
   $("#wins").text(wins);
   $("#loss").text(loss);
-  for (var i = 0; i < numberOptions.length; i++) {
-    var imageCrystal = $("<img>");
-    imageCrystal.addClass("crystal-image");
-    imageCrystal.attr("src", "assets/img/gem" + [i] + ".png");
-    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-    $("#crystals").append(imageCrystal);
-  }
-  $(".crystal-image").on("click", function() {
-    
+  $("#score").text(counter);
+  game();
+}
+
+
+function game() {
+  counter = 0;
+  $("#score").text(counter);
+  var targetNumber = Math.floor(Math.random() * (120 - 19)) + 19;
+  $("#number-to-guess").text(targetNumber);
+  console.log("counter preclick:" + counter);
+  $(".crystal-image").unbind("click").bind("click", function() {
     var crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
     counter += crystalValue;
+    console.log("crystal val" + crystalValue);
+    console.log("counter post:" + counter);
+    console.log("target:" + targetNumber);
     $("#score").text(counter);
     if (counter === targetNumber) {
       wins++;
@@ -32,3 +50,9 @@ var targetNumber = Math.floor(Math.random() * (120 - 19)) + 19;
       alert("You lose!!");
     }
   });
+}
+
+  
+
+
+  
